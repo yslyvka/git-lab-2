7e6849b5b730d0e2da0b818dac083ac8562d33f1
Yuriy Slyvka <ys987821@ohio.edu>
2023-01-25 19:24:56 -0500

    Adding c++ file

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Yuriy Slyvka
+ *        Date: 01/25/2023
+ * Description: Git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
